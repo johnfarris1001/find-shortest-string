34156ca047e4af440063da5d53123a87b50ec995
John <johnfarris217@gmail.com>
2023-01-22 18:50:14 -0600

    initial solution

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,9 +1,29 @@
 function findShortestString(arr) {
   // type your code here
+  let lengths = []
+  for (let i = 0; i < arr.length; i++) {
+    lengths.push(arr[i].length)
+  }
+  return arr[lengths.indexOf(Math.min(...lengths))]
 }
 
 if (require.main === module) {
   // add your own tests in here
+  console.log("Expecting: 'houston'");
+  console.log("=>", findShortestString(['scranton', 'philadelphia', 'west lafayette', 'houston']));
+
+  console.log("");
+
+  console.log("Expecting: 'a'");
+  console.log("=>", findShortestString(['a']));
+
+  console.log("");
+
+  console.log("Expecting: 'tily'");
+  console.log("=>", findShortestString(['floatilla', 'tortilla', 'manilla', 'caterpilla', 'tily', 'stella', 'walnut', 'creek', 'afls']));
+
+  console.log("");
+
   console.log("Expecting: 'a'");
   console.log("=>", findShortestString(['aaa', 'a', 'bb', 'ccc']));
 
@@ -18,6 +38,32 @@ if (require.main === module) {
   console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
 
   // BENCHMARK HERE
+  const startTime = Date.now()
+  const smallInput = ['cecy', 'tilly', 'stella']
+
+  const longInput = []
+
+  for (let i = 0; i < 1000; i++) {
+    let str = ''
+    for (let j = 0; j < 1000; j++) {
+      if (Math.random() < 0.1) {
+        str = str + 'a'
+      }
+    }
+
+    longInput.push(str)
+  }
+  console.log(longInput)
+
+  for (let i = 0; i < 10000; i++) {
+    findShortestString(smallInput)
+    findShortestString(longInput)
+  }
+
+  const averageTime = (Date.now() - startTime) / 2000
+
+  console.log(averageTime)
+
 }
 
 module.exports = findShortestString;
